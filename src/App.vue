350f0cc72aaf6ac8d65bbc661b81da9a0f02b85c
Jeff Chiu <shadow3x3x3@gmail.com>
2018-09-12 16:28:11 +0800

    [Layout] Add nav bar by vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" class="single-page">
+    <navbar/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/login">Login</router-link> |
@@ -9,6 +10,17 @@
     <router-view class="flex-center"></router-view>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
+
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Open+Sans');
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-toolbar dark color="blue">
+    <v-toolbar-side-icon class="sidebar-title">UMN</v-toolbar-side-icon>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar-title {
+  padding-left: 20px;
+  font-size: 30px;
+}
+</style>
+
+
