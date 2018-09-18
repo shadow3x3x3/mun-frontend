e830ed28df16ff443f0abaffcfc7a7bdf1b86b87
Jeff Chiu <shadow3x3x3@gmail.com>
2018-09-18 17:29:16 +0800

    [Layout] Basic card view with RWD

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,35 @@
 <template>
-  <v-container>
-    <h1>This is a dashboard page</h1>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>Located two hours south of Sydney in the.</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+
+      <v-flex>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>Located two hours south of Sydney in the.</div>
+            </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
