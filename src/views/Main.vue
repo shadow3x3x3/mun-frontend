969703b70d0c175345918dc9956652abb0967429
Jeff Chiu <shadow3x3x3@gmail.com>
2018-10-02 15:05:26 +0800

    [Layout] Add Committees table to Main view

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,51 +1,87 @@
 <template>
-  <div class="flex-container">
-    <div class="flex-column">
-      <h1>This is a main page.</h1>
-
-      <h2>Committees</h2>
-      <div class="center-table">
-        <table>
-          <thead>
-            <tr>
-              <th>Committee Name</th>
-              <th>Abbreviation</th>
-              <th>Attached Conference</th>
-              <th>Status</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(cInfo, index) in cInfos" :key="cInfo.id">
-              <td>{{ cInfo.name }}</td>
-              <td>{{ cInfo.abr }}</td>
-              <td>{{ cInfo.ac }}</td>
-              <td>{{ cInfo.status }}</td>
-              <td>
-                <a class="button">{{ index }}</a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+  <v-container grid-list-md>
+    <v-layout column wrap justify-center>
+      <v-flex>
+        <h2>Committees</h2>
+      </v-flex>
+      <v-flex>
+        <v-data-table
+          :headers="headers"
+          :items="committees"
+          class="elevation-1"
+        >
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.name }}</td>
+            <td class="text-xs-left">{{ props.item.abbr }}</td>
+            <td class="text-xs-left">{{ props.item.conference }}</td>
+            <td class="text-xs-left">{{ props.item.status }}</td>
+            <td class="text-xs-left"> Button Placeholder </td>
+          </template>
+          <template slot="no-data">
+            <v-alert :value="true" color="error" icon="warning">
+              Sorry, nothing to display here :(
+            </v-alert>
+          </template>
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      cInfos: [
+      headers: [
         {
-          id: 123, name: 'A', abr: 'B', ac: 'B', status: 'None',
+          text: 'Committee Name',
+          align: 'left',
+          value: 'name',
+        },
+        {
+          text: 'Abbreviation',
+          align: 'left',
+          value: 'abbr',
+        },
+        {
+          text: 'Attached Conference',
+          align: 'left',
+          value: 'conference',
+        },
+        {
+          text: 'Status',
+          align: 'left',
+          value: 'status',
+          width: '10%',
+        },
+        {
+          text: ' Edit',
+          align: 'left',
+          value: 'edit',
+          width: '10%',
+        },
+      ],
+      committees: [
+        {
+          name: 'MUN Club Trial',
+          abbr: 'MUN Club Trial abbr',
+          conference: 'PAWNU',
+          status: 'true',
+        },
+        {
+          name: 'A Club Trial',
+          abbr: 'A Trial abbr',
+          conference: 'ANU',
+          status: 'true',
+        },
+        {
+          name: 'B Club Trial',
+          abbr: 'B Club Trial abbr',
+          conference: 'BNU',
+          status: 'true',
         },
       ],
     };
   },
 };
 </script>
-
-
-<style lang="scss" scoped>
-</style>
