2b954d9bc8fcbe7e33ce0097277fc1a91fff1b97
Jeff Chiu <shadow3x3x3@gmail.com>
2018-10-08 16:37:10 +0800

    [Layout] Clean home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,5 @@
 <template>
   <v-container fluid>
-    <v-layout column>
-      <v-flex xs12>
-        <div class="resultContainer">
-          <v-layout v-bind="layoutAttributes">
-            <div class="item elevation-5"></div>
-            <div class="item elevation-5"></div>
-            <div class="item elevation-5"></div>
-          </v-layout>
-        </div>
-      </v-flex>
-      <v-flex xs12>
-        <v-layout row xs12 wrap>
-          <v-flex xs12 md4>
-            <h1>123</h1>
-          </v-flex>
-          <v-flex xs12 md4>
-            <h2>234</h2>
-          </v-flex>
-          <v-flex xs12 md4>
-            <v-layout column>
-              <h3>123456</h3>
-            </v-layout>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-      <v-flex xs12>
-        <h5>Output:</h5>
-      </v-flex>
-    </v-layout>
   </v-container>
 </template>
 
